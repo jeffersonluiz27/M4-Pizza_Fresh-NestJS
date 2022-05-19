438250f54e6e8777a7767a3be238d59f231fa1d3
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-05-18 22:20:26 -0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('PizzaFresh')
+    .setDescription('Aplicação para gestão das mesas de uma pizzaria')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('table')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3333);
 }
--- src/table/dto/create-table.dto.ts
+++ src/table/dto/create-table.dto.ts
@@ -1,7 +1,12 @@
 import { IsPositive, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTableDto {
   @IsPositive()
   @IsNumber()
+  @ApiProperty({
+    description: 'O número da mesa',
+    example: 1,
+  })
   number: number;
 }
