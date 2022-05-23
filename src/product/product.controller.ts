9d724d4a2b49819b3438294c050c30a1544aa868
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-05-23 20:07:32 -0300

    criando product

---
+++ src/product/product.controller.ts
@@ -0,0 +1,62 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+
+@ApiTags('product')
+@Controller('product')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: 'Criar um produto',
+  })
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productService.create(createProductDto);
+  }
+
+  @Get()
+  @ApiOperation({
+    summary: 'Listar todos os produtos',
+  })
+  findAll() {
+    return this.productService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Listar um produto pelo ID',
+  })
+  findOne(@Param('id') id: string) {
+    return this.productService.findOne(+id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({
+    summary: 'Atualizar um produto pelo ID',
+  })
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productService.update(+id, updateProductDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    summary: 'Deletar um produto pelo ID',
+  })
+  remove(@Param('id') id: string) {
+    return this.productService.remove(+id);
+  }
+}
